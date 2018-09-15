0dfed4acd3984dd5dbd950437e2e054cbf73b3e3
aki_ks <aki@kaysubs.de>
2018-09-15 11:40:28 +0000

    Add return instruction

---
+++ stack/src/main/java/me/aki/tactical/stack/insn/ReturnInsn.java
@@ -0,0 +1,44 @@
+package me.aki.tactical.stack.insn;
+
+import me.aki.tactical.core.type.Type;
+
+import java.util.Optional;
+
+/**
+ * Return from a method (java's 'return' keyword).
+ *
+ * If the method is not of 'void' type, a value will
+ * be popped from the stack and returned.
+ */
+public class ReturnInsn implements Instruction {
+    /**
+     * Type that is returned by the method or empty for 'void' methods.
+     */
+    private Optional<Type> type;
+
+    /**
+     * Initialize a return instruction that returns no value.
+     */
+    public ReturnInsn() {
+        this.type = Optional.empty();
+    }
+
+    /**
+     * Initialize a return instruction that returns a value.
+     *
+     * @param type of returned value
+     */
+    public ReturnInsn(Type type) {
+        this.type = Optional.of(type);
+    }
+
+    @Override
+    public int getPushCount() {
+        return 0;
+    }
+
+    @Override
+    public int getPopCount() {
+        return type.isPresent() ? 1 : 0;
+    }
+}
