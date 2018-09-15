e40c57f82578513b6db9a9a10560aef1fa42a54f
aki_ks <aki@kaysubs.de>
2018-09-15 17:54:07 +0000

    Add swap instruction

---
+++ stack/src/main/java/me/aki/tactical/stack/insn/SwapInsn.java
@@ -0,0 +1,22 @@
+package me.aki.tactical.stack.insn;
+
+/**
+ * Pop the two upper values on the stack and push them in the opposite order.
+ *
+ * This instruction cannot swap long or double values.
+ *
+ * example:
+ * - before: value1, value2, ...
+ * - after:  value2, value1, ...
+ */
+public class SwapInsn implements Instruction {
+    @Override
+    public int getPushCount() {
+        return 2;
+    }
+
+    @Override
+    public int getPopCount() {
+        return 2;
+    }
+}
