af15e09cb816294efd08961236a21634c51db988
aki_ks <aki@kaysubs.de>
2018-09-16 17:28:12 +0000

    Add switch instruction

---
+++ stack/src/main/java/me/aki/tactical/stack/insn/SwitchInsn.java
@@ -0,0 +1,58 @@
+package me.aki.tactical.stack.insn;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * An int value is popped from the stack and checked against a table of branch locations.
+ *
+ * This instruction replaces the TABLE_SWITCH and LOOKUP_SWITCH opcode.
+ */
+public class SwitchInsn implements BranchInsn {
+    /**
+     * Mapping from int constants to their corresponding branch location.
+     */
+    private Map<Integer, Instruction> branchTable = new LinkedHashMap<>();
+
+    /**
+     * The default branch location if a popped value is not contained
+     * in the {@link SwitchInsn#branchTable} map.
+     */
+    private Instruction defaultLocation;
+
+    public SwitchInsn(Map<Integer, Instruction> branchTable, Instruction defaultLocation) {
+        this.branchTable = branchTable;
+        this.defaultLocation = defaultLocation;
+    }
+
+    public Map<Integer, Instruction> getBranchTable() {
+        return branchTable;
+    }
+
+    public void setBranchTable(Map<Integer, Instruction> branchTable) {
+        this.branchTable = branchTable;
+    }
+
+    public Instruction getDefaultLocation() {
+        return defaultLocation;
+    }
+
+    public void setDefaultLocation(Instruction defaultLocation) {
+        this.defaultLocation = defaultLocation;
+    }
+
+    @Override
+    public int getPushCount() {
+        return 0;
+    }
+
+    @Override
+    public int getPopCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean continuesExecution() {
+        return false;
+    }
+}
