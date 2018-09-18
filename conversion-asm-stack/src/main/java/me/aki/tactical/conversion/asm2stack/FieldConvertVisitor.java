2bde124c6deff4926fd8288091211b224d7e133f
aki_ks <aki@kaysubs.de>
2018-09-18 21:55:38 +0000

    Convert type & regular annotations of fields

--- conversion-asm-stack/src/main/java/me/aki/tactical/conversion/asm2stack/FieldConvertVisitor.java
+++ conversion-asm-stack/src/main/java/me/aki/tactical/conversion/asm2stack/FieldConvertVisitor.java
@@ -1,12 +1,17 @@
 package me.aki.tactical.conversion.asm2stack;
 
 import me.aki.tactical.core.Field;
+import me.aki.tactical.core.annotation.Annotation;
+import me.aki.tactical.core.typeannotation.FieldTypeAnnotation;
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.Attribute;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.TypePath;
 
+/**
+ * Visitor that converts all events and applies them to a {@link Field}.
+ */
 public class FieldConvertVisitor extends FieldVisitor {
     private final Field field;
 
@@ -17,14 +22,24 @@ public class FieldConvertVisitor extends FieldVisitor {
 
     @Override
     public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
-//        return super.visitAnnotation(descriptor, visible);
-        throw new RuntimeException("Not yet implemented");
+        Annotation annotation = new Annotation(AsmUtil.pathFromObjectDescriptor(descriptor), visible);
+
+        this.field.getAnnotations().add(annotation);
+
+        AnnotationVisitor av = super.visitAnnotation(descriptor, visible);
+        av = new AnnotationConvertVisitor(av, annotation);
+        return av;
     }
 
     @Override
     public AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath, String descriptor, boolean visible) {
-//        return super.visitTypeAnnotation(typeRef, typePath, descriptor, visible);
-        throw new RuntimeException("Not yet implemented");
+        Annotation annotation = new Annotation(AsmUtil.pathFromObjectDescriptor(descriptor), visible);
+
+        this.field.getTypeAnnotations().add(new FieldTypeAnnotation(AsmUtil.fromAsmTypePath(typePath), annotation));
+
+        AnnotationVisitor av = super.visitTypeAnnotation(typeRef, typePath, descriptor, visible);
+        av = new AnnotationConvertVisitor(av, annotation);
+        return av;
     }
 
     @Override
