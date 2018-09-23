e8acce6903a61625a140283b2ce1f77d18856b75
aki_ks <aki@kaysubs.de>
2018-09-23 23:13:38 +0000

    Convert field annotations

--- conversion-asm-stack/src/main/java/me/aki/tactical/conversion/stack2asm/TacticalFieldReader.java
+++ conversion-asm-stack/src/main/java/me/aki/tactical/conversion/stack2asm/TacticalFieldReader.java
@@ -1,6 +1,8 @@
 package me.aki.tactical.conversion.stack2asm;
 
 import me.aki.tactical.core.Field;
+import me.aki.tactical.core.annotation.Annotation;
+import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.FieldVisitor;
 
 public class TacticalFieldReader {
@@ -11,6 +13,19 @@ public class TacticalFieldReader {
     }
 
     public void accept(FieldVisitor fv) {
+        visitAnnotations(fv);
         fv.visitEnd();
+    }
+
+    private void visitAnnotations(FieldVisitor fv) {
+        for (Annotation annotation : field.getAnnotations()) {
+            String descriptor = AsmUtil.pathToDescriptor(annotation.getType());
+            boolean isVisible = annotation.isRuntimeVisible();
+
+            AnnotationVisitor av = fv.visitAnnotation(descriptor, isVisible);
+            if (av != null) {
+                new TacticalAnnotationReader(annotation).accept(av);
+            }
+        }
     }
 }
