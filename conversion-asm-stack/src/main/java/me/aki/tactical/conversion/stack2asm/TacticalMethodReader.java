34ba437f8bf8b8130d70244abd59af16e14f866d
aki_ks <aki@kaysubs.de>
2018-09-24 10:29:04 +0000

    Convert method type annotations

--- conversion-asm-stack/src/main/java/me/aki/tactical/conversion/stack2asm/TacticalMethodReader.java
+++ conversion-asm-stack/src/main/java/me/aki/tactical/conversion/stack2asm/TacticalMethodReader.java
@@ -4,8 +4,12 @@ import me.aki.tactical.conversion.stackasm.AccessConverter;
 import me.aki.tactical.core.Method;
 import me.aki.tactical.core.annotation.Annotation;
 import me.aki.tactical.core.annotation.AnnotationValue;
+import me.aki.tactical.core.typeannotation.MethodTypeAnnotation;
+import me.aki.tactical.core.typeannotation.TargetType;
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.TypePath;
+import org.objectweb.asm.TypeReference;
 
 import java.util.LinkedHashMap;
 
@@ -20,6 +24,7 @@ public class TacticalMethodReader {
         visitParameters(mv);
         visitAnnotationDefault(mv);
         visitAnnotations(mv);
+        visitTypeAnnotations(mv);
         mv.visitEnd();
     }
 
@@ -52,6 +57,45 @@ public class TacticalMethodReader {
             if (av != null) {
                 new TacticalAnnotationReader(annotation.getValues()).accept(av);
             }
+        }
+    }
+
+    private void visitTypeAnnotations(MethodVisitor cv) {
+        for (MethodTypeAnnotation typeAnnotation : method.getTypeAnnotations()) {
+            Annotation annotation = typeAnnotation.getAnnotation();
+
+            int typeRef = convertTargetType(typeAnnotation.getTargetType()).getValue();
+            TypePath typePath = AsmUtil.toAsmTypePath(typeAnnotation.getTypePath());
+            String descriptor = AsmUtil.pathToDescriptor(annotation.getType());
+            boolean isVisible = annotation.isRuntimeVisible();
+
+            AnnotationVisitor av = cv.visitTypeAnnotation(typeRef, typePath, descriptor, isVisible);
+            if (av != null) {
+                new TacticalAnnotationReader(annotation.getValues()).accept(av);
+            }
+        }
+    }
+
+    private TypeReference convertTargetType(TargetType.MethodTargetType targetType) {
+        if (targetType instanceof TargetType.CheckedException) {
+            int exceptionIndex = ((TargetType.CheckedException) targetType).getException();
+            return TypeReference.newExceptionReference(exceptionIndex);
+        } else if (targetType instanceof TargetType.MethodParameter) {
+            int parameterIndex = ((TargetType.MethodParameter) targetType).getParameter();
+            return TypeReference.newFormalParameterReference(parameterIndex);
+        } else if (targetType instanceof TargetType.MethodReceiver) {
+            return TypeReference.newTypeReference(TypeReference.METHOD_RECEIVER);
+        } else if (targetType instanceof TargetType.ReturnType) {
+            return TypeReference.newTypeReference(TypeReference.METHOD_RETURN);
+        } else if (targetType instanceof TargetType.TypeParameter) {
+            int parameterIndex = ((TargetType.TypeParameter) targetType).getParameterIndex();
+            return TypeReference.newTypeParameterReference(TypeReference.METHOD_TYPE_PARAMETER, parameterIndex);
+        } else if (targetType instanceof TargetType.TypeParameterBound) {
+            int parameterIndex = ((TargetType.TypeParameterBound) targetType).getParameterIndex();
+            int boundIndex = ((TargetType.TypeParameterBound) targetType).getBoundIndex();
+            return TypeReference.newTypeParameterBoundReference(TypeReference.METHOD_TYPE_PARAMETER_BOUND, parameterIndex, boundIndex);
+        } else {
+            throw new AssertionError();
         }
     }
 }
