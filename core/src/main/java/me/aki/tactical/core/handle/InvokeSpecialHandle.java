15240c2384dcc277b1f7620cc0d79669264994fa
aki_ks <aki@kaysubs.de>
2018-09-25 01:23:32 +0000

    Fix compile error in method handle implementations

--- core/src/main/java/me/aki/tactical/core/handle/InvokeSpecialHandle.java
+++ core/src/main/java/me/aki/tactical/core/handle/InvokeSpecialHandle.java
@@ -1,9 +1,14 @@
 package me.aki.tactical.core.handle;
 
+import me.aki.tactical.core.InvokableMethodRef;
 import me.aki.tactical.core.MethodRef;
 
 public class InvokeSpecialHandle extends AbstractAmbiguousMethodHandle {
     public InvokeSpecialHandle(MethodRef methodRef, boolean isInterface) {
-        super(methodRef, isInterface);
+        super(new InvokableMethodRef(methodRef.getOwner(), methodRef.getName(), methodRef.getArguments(), methodRef.getReturnType(), isInterface));
+    }
+
+    public InvokeSpecialHandle(InvokableMethodRef methodRef) {
+        super(methodRef);
     }
 }
--- core/src/main/java/me/aki/tactical/core/handle/InvokeStaticHandle.java
+++ core/src/main/java/me/aki/tactical/core/handle/InvokeStaticHandle.java
@@ -1,9 +1,14 @@
 package me.aki.tactical.core.handle;
 
+import me.aki.tactical.core.InvokableMethodRef;
 import me.aki.tactical.core.MethodRef;
 
 public class InvokeStaticHandle extends AbstractAmbiguousMethodHandle implements BootstrapMethodHandle {
     public InvokeStaticHandle(MethodRef methodRef, boolean isInterface) {
-        super(methodRef, isInterface);
+        super(new InvokableMethodRef(methodRef.getOwner(), methodRef.getName(), methodRef.getArguments(), methodRef.getReturnType(), isInterface));
+    }
+
+    public InvokeStaticHandle(InvokableMethodRef methodRef) {
+        super(methodRef);
     }
 }
