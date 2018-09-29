597a5c671960389668bbacfe7dad1b0149c89b62
aki_ks <aki@kaysubs.de>
2018-09-29 16:33:29 +0000

    Implement assignment statement

---
+++ ref/src/main/java/me/aki/tactical/ref/stmt/AssignStatement.java
@@ -0,0 +1,57 @@
+package me.aki.tactical.ref.stmt;
+
+import me.aki.tactical.core.util.Cell;
+import me.aki.tactical.ref.Expression;
+import me.aki.tactical.ref.Statement;
+import me.aki.tactical.ref.Variable;
+
+import java.util.List;
+
+/**
+ * Assign a value to a variable
+ */
+public class AssignStatement implements Statement {
+    /**
+     * Variable that will be reassigned
+     */
+    private Variable variable;
+
+    /**
+     * Value to be assigned to the variable.
+     */
+    private Expression value;
+
+    public AssignStatement(Variable variable, Expression value) {
+        this.variable = variable;
+        this.value = value;
+    }
+
+    public Variable getVariable() {
+        return variable;
+    }
+
+    public void setVariable(Variable variable) {
+        this.variable = variable;
+    }
+
+    public Cell<Variable> getVariableCell() {
+        return Cell.of(this::getVariable, this::setVariable);
+    }
+
+    public Expression getValue() {
+        return value;
+    }
+
+    public void setValue(Expression value) {
+        this.value = value;
+    }
+
+    public Cell<Expression> getValueCell() {
+        return Cell.of(this::getValue, this::setValue);
+    }
+
+    @Override
+    public List<Cell<Expression>> getReferencedValues() {
+        return List.of(getVariableCell(), getValueCell());
+    }
+}
