23fb6afe030e996d2a4654ccb2aba5c1980f63f6
aki_ks <aki@kaysubs.de>
2018-09-30 22:28:23 +0000

    Merge ConversionContext into BodyConverter

--- conversion-stack-ref/src/main/java/me/aki/tactical/conversion/stack2ref/BodyConverter.java
+++ conversion-stack-ref/src/main/java/me/aki/tactical/conversion/stack2ref/BodyConverter.java
@@ -1,10 +1,17 @@
 package me.aki.tactical.conversion.stack2ref;
 
+import me.aki.tactical.core.util.Cell;
 import me.aki.tactical.ref.RefBody;
 import me.aki.tactical.ref.RefLocal;
+import me.aki.tactical.ref.Statement;
 import me.aki.tactical.stack.StackBody;
 import me.aki.tactical.stack.StackLocal;
+import me.aki.tactical.stack.insn.Instruction;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -13,16 +20,37 @@ import java.util.stream.Collectors;
 public class BodyConverter {
     private final StackBody stackBody;
     private final RefBody refBody;
-    private final ConversionContext ctx;
+
+    /**
+     * Map locals in stack representation to the corresponding ref locals.
+     */
+    private final Map<StackLocal, RefLocal> localMap = new HashMap<>();
+
+    /**
+     * Cells that should be assigned to the statement corresponding to a certain instructions.
+     */
+    private final Map<Instruction, List<Cell<Statement>>> instructionReferences = new HashMap<>();
 
     public BodyConverter(StackBody stackBody) {
         this.stackBody = stackBody;
         this.refBody = new RefBody();
-        this.ctx = new ConversionContext(stackBody);
     }
 
     public RefBody getRefBody() {
         return refBody;
+    }
+
+    public void registerInsnReference(Instruction instruction, Cell<Statement> statementCell) {
+        List<Cell<Statement>> cells = this.instructionReferences.computeIfAbsent(instruction, x -> new ArrayList<>());
+        cells.add(statementCell);
+    }
+
+    public Map<Instruction, List<Cell<Statement>>> getInstructionReferences() {
+        return instructionReferences;
+    }
+
+    public RefLocal getLocal(StackLocal local) {
+        return this.localMap.get(local);
     }
 
     public void convert() {
@@ -33,12 +61,12 @@ public class BodyConverter {
         for (StackLocal stackLocal : stackBody.getLocals()) {
             RefLocal refLocal = new RefLocal(null);
             refBody.getLocals().add(refLocal);
-            ctx.getLocalMap().put(stackLocal, refLocal);
+            localMap.put(stackLocal, refLocal);
         }
 
-        refBody.setThisLocal(stackBody.getThisLocal().map(ctx::getLocal));
+        refBody.setThisLocal(stackBody.getThisLocal().map(this::getLocal));
         refBody.setArgumentLocals(stackBody.getParameterLocals().stream()
-                .map(ctx::getLocal)
+                .map(this::getLocal)
                 .collect(Collectors.toList()));
     }
 }
