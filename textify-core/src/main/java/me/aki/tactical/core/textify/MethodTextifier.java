5a20a9a34ba0b31429e85978750a0d544091f1c3
aki_ks <aki@kaysubs.de>
2018-10-06 12:34:34 +0000

    Implement a basic method textifier

--- textify-core/src/main/java/me/aki/tactical/core/textify/MethodTextifier.java
+++ textify-core/src/main/java/me/aki/tactical/core/textify/MethodTextifier.java
@@ -1,6 +1,13 @@
 package me.aki.tactical.core.textify;
 
+import me.aki.tactical.core.Attribute;
 import me.aki.tactical.core.Method;
+import me.aki.tactical.core.Path;
+import me.aki.tactical.core.annotation.Annotation;
+import me.aki.tactical.core.typeannotation.MethodTypeAnnotation;
+
+import java.util.List;
+import java.util.Optional;
 
 public class MethodTextifier implements Textifier<Method> {
     private static final MethodTextifier INSTANCE = new MethodTextifier();
@@ -13,6 +20,84 @@ public class MethodTextifier implements Textifier<Method> {
 
     @Override
     public void textify(Printer printer, Method method) {
-        throw new RuntimeException("Not yet implemented");
+        appendSignature(printer, method.getSignature());
+        appendAnnotations(printer, method.getAnnotations());
+        appendTypeAnnotations(printer, method.getTypeAnnotations());
+        appendAttributes(printer, method.getAttributes());
+
+        FlagTextifier.METHOD.textify(printer, method.getAccessFlags());
+        method.getReturnType().ifPresentOrElse(
+                type -> TypeTextifier.getInstance().textify(printer, type),
+                () -> printer.addText("void"));
+        printer.addText(" ");
+        printer.addLiteral(method.getName());
+        printer.addText("(");
+        appendArguments(printer, method);
+        printer.addText(")");
+
+        List<Path> exceptions = method.getExceptions();
+        if (!exceptions.isEmpty()) {
+            printer.addText(" throws ");
+            exceptions.forEach(printer::addPath);
+        }
+
+        method.getDefaultValue().ifPresent(defaultValue -> {
+            printer.addText(" = ");
+            AnnotationValueTextifier.getInstance().textify(printer, defaultValue);
+        });
+
+        method.getBody().ifPresentOrElse(body -> {
+            printer.addText(" {");
+            printer.newLine();
+            printer.increaseIndent();
+
+            printer.decreaseIndent();
+            printer.addText("}");
+            printer.newLine();
+        }, () -> {
+            printer.addText(";");
+        });
+    }
+
+    private void appendSignature(Printer printer, Optional<String> signatureOpt) {
+        signatureOpt.ifPresent(signature -> {
+            printer.addText("signature ");
+            printer.addEscaped(signature, '"');
+            printer.addText(";");
+            printer.newLine();
+        });
+    }
+
+    private void appendAnnotations(Printer printer, List<Annotation> annotations) {
+        for (Annotation annotation : annotations) {
+            AnnotationTextifier.getInstance().textify(printer, annotation);
+            printer.newLine();
+        }
+    }
+
+    private void appendTypeAnnotations(Printer printer, List<MethodTypeAnnotation> typeAnnotations) {
+        for (MethodTypeAnnotation typeAnnotation : typeAnnotations) {
+            printer.addText("#[path = ");
+            TypePathTextifier.getInstance().textify(printer, typeAnnotation.getTypePath());
+            printer.addText(", target = ");
+            TargetTypeTextifier.METHOD_TARGET_TYPE.textify(printer, typeAnnotation.getTargetType());
+            printer.addText(", annotation = ");
+            AnnotationTextifier.getInstance().textify(printer, typeAnnotation.getAnnotation());
+            printer.addText("]");
+            printer.newLine();
+        }
+    }
+
+    private void appendAttributes(Printer printer, List<Attribute> attributes) {
+        for (Attribute attribute : attributes) {
+            AttributeTextifier.getInstance().textify(printer, attribute);
+            printer.newLine();
+        }
+    }
+
+    private void appendArguments(Printer printer, Method method) {
+        TextUtil.joined(method.getParameterTypes(),
+                type -> TypeTextifier.getInstance().textify(printer, type),
+                () -> printer.addText(", "));
     }
 }
