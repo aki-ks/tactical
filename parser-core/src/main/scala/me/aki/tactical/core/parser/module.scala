e6dd90f75875ba5be87b5e0c2bd9e4fa2a8529fb
aki_ks <aki@kaysubs.de>
2018-10-09 14:51:04 +0000

    Implement Module parsers

---
+++ parser-core/src/main/scala/me/aki/tactical/core/parser/module.scala
@@ -0,0 +1,128 @@
+package me.aki.tactical.core.parser
+
+import scala.collection.JavaConverters._
+
+import java.util.Optional
+
+import fastparse.all._
+import me.aki.tactical.core.{ Module, Path }
+
+object ModuleParser extends Parser[Module] {
+  val parser: P[Module] = P {
+    ModuleAccessParser ~ WS.? ~ "module" ~ WS ~ PathParser ~ WS.? ~ "{" ~ WS.? ~
+      ModuleContentParser.rep(sep = WS.?) ~ WS.? ~ "}"
+  } map {
+    case (flags, name, contents) =>
+      val module = new Module(name)
+      module.setAccessFlags(flags)
+      contents.foreach(_ apply module)
+      module
+  }
+}
+
+trait ModuleContent extends (Module => Unit)
+object ModuleContent {
+  class Version(version: String) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.setVersion(Optional.of(version))
+  }
+
+  class Main(main: Path) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.setMainClass(Optional.of(main))
+  }
+
+  class Package(pkg: Path) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.getPackages.add(pkg)
+  }
+
+  class Require(require: Module.Require) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.getRequires.add(require)
+  }
+
+  class Export(export: Module.Export) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.getExports.add(export)
+  }
+
+  class Opens(opens: Module.Open) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.getOpens.add(opens)
+  }
+
+  class Use(use: Path) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.getUses.add(use)
+  }
+
+  class Provides(provides: Module.Provide) extends ModuleContent {
+    override def apply(module: Module): Unit =
+      module.getProvides.add(provides)
+  }
+}
+
+object ModuleContentParser extends Parser[ModuleContent] {
+  object VersionParser extends Parser[ModuleContent.Version] {
+    val parser: P[ModuleContent.Version] =
+      for (version ← "version" ~ WS ~ StringLiteral ~ WS.? ~ ";")
+        yield new ModuleContent.Version(version)
+  }
+
+  object MainParser extends Parser[ModuleContent.Main] {
+    val parser: P[ModuleContent.Main] =
+      for (version ← "main" ~ WS ~ PathParser ~ WS.? ~ ";")
+        yield new ModuleContent.Main(version)
+  }
+
+  object PackageParser extends Parser[ModuleContent.Package] {
+    val parser: P[ModuleContent.Package] =
+      for (version ← "package" ~ WS ~ PathParser ~ WS.? ~ ";")
+        yield new ModuleContent.Package(version)
+  }
+
+  object RequireParser extends Parser[ModuleContent.Require] {
+    val parser: P[ModuleContent.Require] =
+      for ((flags, name, version) ← ModuleRequireAccessParser ~ "requires" ~ WS ~ PathParser ~ WS.? ~ (":" ~ WS.? ~ StringLiteral ~ WS.?).? ~ ";")
+        yield new ModuleContent.Require(new Module.Require(name, flags, Optional.ofNullable(version.orNull)))
+  }
+
+  object ExportParser extends Parser[ModuleContent.Export] {
+    val parser: P[ModuleContent.Export] =
+      for ((flags, name, modules) ← ModuleExportAccessParser ~ "exports" ~ WS.? ~ PathParser ~ WS.? ~
+        ("to" ~ WS.? ~ PathParser.rep(min = 1, sep = WS.? ~ "," ~ WS.?) ~ WS.?).? ~ ";")
+        yield new ModuleContent.Export(new Module.Export(name, flags, modules.getOrElse(Nil).asJava))
+  }
+
+  object OpensParser extends Parser[ModuleContent.Opens] {
+    val parser: P[ModuleContent.Opens] =
+      for ((flags, name, modules) ← ModuleOpensAccessParser ~ "opens" ~ WS.? ~ PathParser ~ WS.? ~
+        ("to" ~ WS.? ~ PathParser.rep(min = 1, sep = WS.? ~ "," ~ WS.?) ~ WS.?).? ~ ";")
+        yield new ModuleContent.Opens(new Module.Open(name, flags, modules.getOrElse(Nil).asJava))
+  }
+
+  object UsesParser extends Parser[ModuleContent.Use] {
+    val parser: P[ModuleContent.Use] =
+      for (path ← "uses" ~ WS.? ~ PathParser ~ WS.? ~ ";")
+        yield new ModuleContent.Use(path)
+  }
+
+  object ProvidesParser extends Parser[ModuleContent.Provides] {
+    val parser: P[ModuleContent.Provides] =
+      for ((service, providers) ← "provides" ~ WS.? ~ PathParser ~ WS.? ~
+        ("with" ~ WS.? ~ PathParser.rep(min = 1, sep = WS.? ~ "," ~ WS.?) ~ WS.?).? ~ ";")
+        yield new ModuleContent.Provides(new Module.Provide(service, providers.getOrElse(Nil).asJava))
+  }
+
+  val parser: P[ModuleContent] = P {
+    VersionParser |
+      MainParser |
+      PackageParser |
+      UsesParser |
+      ProvidesParser |
+      RequireParser |
+      ExportParser |
+      OpensParser
+  }
+}
---
+++ parser-core/src/test/scala/me/aki/tactical/core/parser/test/ModuleTest.scala
@@ -0,0 +1,55 @@
+package me.aki.tactical.core.parser.test
+
+import scala.collection.JavaConverters._
+
+import java.util.{ Set => JSet, List => JList, Optional }
+
+import me.aki.tactical.core.{Module, Path}
+import me.aki.tactical.core.parser.ModuleParser
+import org.scalatest.{FlatSpec, Matchers}
+
+class ModuleTest extends FlatSpec with Matchers {
+  "The ModuleParser" should "parse a basic module skeleton" in {
+    ModuleParser.parse("module java.lang.base {}") shouldEqual
+      new Module(Path.of("java", "lang", "base"))
+  }
+
+  it should "parse module versions" in {
+    ModuleParser.parse("module foo { version \"1.0\"; }").getVersion shouldEqual Optional.of("1.0")
+  }
+
+  it should "parse main classes" in {
+    ModuleParser.parse("module foo { main com.example.Main; }").getMainClass shouldEqual Optional.of(Path.of("com", "example", "Main"))
+  }
+
+  it should "parse packages" in {
+    ModuleParser.parse("module foo { package com.example.foo; }").getPackages shouldEqual List(Path.of("com", "example", "foo")).asJava
+  }
+
+  it should "parse requires" in {
+    ModuleParser.parse("module foo { requires com.example.foo; }").getRequires shouldEqual
+      List(new Module.Require(Path.of("com", "example", "foo"), JSet.of(), Optional.empty())).asJava
+
+    ModuleParser.parse("module foo { requires com.example.foo : \"4.8\"; }").getRequires shouldEqual
+      List(new Module.Require(Path.of("com", "example", "foo"), JSet.of(), Optional.of("4.8"))).asJava
+  }
+
+  it should "parse exports" in {
+    ModuleParser.parse("module foo { exports com.example.foo to com.example.bar, com.example.baz; }").getExports shouldEqual
+      List(new Module.Export(Path.of("com", "example", "foo"), JSet.of(), JList.of(Path.of("com", "example", "bar"), Path.of("com", "example", "baz")))).asJava
+  }
+
+  it should "parse opens" in {
+    ModuleParser.parse("module foo { opens com.example.foo to com.example.bar, com.example.baz; }").getOpens shouldEqual
+      List(new Module.Open(Path.of("com", "example", "foo"), JSet.of(), JList.of(Path.of("com", "example", "bar"), Path.of("com", "example", "baz")))).asJava
+  }
+
+  it should "parse use" in {
+    ModuleParser.parse("module foo { uses com.example.foo; }").getUses shouldEqual List(Path.of("com", "example", "foo")).asJava
+  }
+
+  it should "parse provides" in {
+    ModuleParser.parse("module foo { provides com.example.foo with com.example.bar, com.example.baz; }").getProvides shouldEqual
+      List(new Module.Provide(Path.of("com", "example", "foo"), JList.of(Path.of("com", "example", "bar"), Path.of("com", "example", "baz")))).asJava
+  }
+}
