b2720bb591fe286f9bb4d4c7c55813cd9ac4c842
aki_ks <aki@kaysubs.de>
2018-10-09 14:31:36 +0000

    Add Access parsers for Module

--- parser-core/src/main/scala/me/aki/tactical/core/parser/access.scala
+++ parser-core/src/main/scala/me/aki/tactical/core/parser/access.scala
@@ -4,13 +4,13 @@ import scala.collection.JavaConverters._
 import java.util.{HashSet => JHashSet, Set => JSet}
 
 import fastparse.all._
-import me.aki.tactical.core.Classfile
+import me.aki.tactical.core.{ Classfile, Module }
 
 class AccessParser[F <: Enum[F]](flags: (String, F)*) extends Parser[JSet[F]] {
   val parser: P[JSet[F]] = {
     val flagParser = {
       for ((name, flag) ← flags)
-        yield P[F] { name.!.map(_ => flag) }
+        yield P[F] { (name ~ WS).!.map(_ => flag) }
     } reduce (_ | _)
 
     flagParser.rep(sep = WS).map(x => new JHashSet(x.asJava))
@@ -23,4 +23,27 @@ object ClassAccessParser extends AccessParser[Classfile.Flag](
   "super" -> Classfile.Flag.SUPER,
   "abstract" -> Classfile.Flag.ABSTRACT,
   "synthetic" -> Classfile.Flag.SYNTHETIC
+)
+
+object ModuleAccessParser extends AccessParser[Module.Flag](
+  "open" -> Module.Flag.OPEN,
+  "synthetic" -> Module.Flag.SYNTHETIC,
+  "mandated" -> Module.Flag.MANDATED
+)
+
+object ModuleRequireAccessParser extends AccessParser[Module.Require.Flag](
+  "transitive" -> Module.Require.Flag.TRANSITIVE,
+  "static-phase" -> Module.Require.Flag.STATIC_PHASE,
+  "synthetic" -> Module.Require.Flag.SYNTHETIC,
+  "mandated" -> Module.Require.Flag.MANDATED
+)
+
+object ModuleExportAccessParser extends AccessParser[Module.Export.Flag](
+  "synthetic" -> Module.Export.Flag.SYNTHETIC,
+  "mandated" -> Module.Export.Flag.MANDATED
+)
+
+object ModuleOpensAccessParser extends AccessParser[Module.Open.Flag](
+  "synthetic" -> Module.Open.Flag.SYNTHETIC,
+  "mandated" -> Module.Open.Flag.MANDATED
 )
