6912b79521a627dafc9083e7841a6ccd937a5381
aki_ks <aki@kaysubs.de>
2018-10-10 13:38:41 +0000

    Implement the classfile parser

---
+++ parser-core/src/main/scala/me/aki/tactical/core/parser/classfile.scala
@@ -0,0 +1,242 @@
+package me.aki.tactical.core.parser
+
+import scala.collection.JavaConverters._
+import java.util.Optional
+
+import fastparse.all._
+import me.aki.tactical.core._
+import me.aki.tactical.core.Classfile.{EnclosingMethod, Version}
+import me.aki.tactical.core.annotation.Annotation
+import me.aki.tactical.core.typeannotation.ClassTypeAnnotation
+
+object ClassfileParser extends Parser[Classfile] {
+  val parser: P[Classfile] = P {
+
+    val pkg = "package" ~ WS ~ Literal.rep(min = 1, sep = WS.? ~ "." ~ WS.?) ~ WS.? ~ ";"
+    val version = "version" ~ WS ~ integerNumber.! ~ WS.? ~ "." ~ WS.? ~ integerNumber.! ~ WS.? ~ ";"
+
+    val classDescriptor = P {
+      val classKeywordParser = Seq(
+        for (_ ← "class".!) yield None,
+        for (_ ← "interface".!) yield Some(Classfile.Flag.INTERFACE),
+        for (_ ← "@interface".!) yield Some(Classfile.Flag.ANNOTATION),
+        for (_ ← "enum".!) yield Some(Classfile.Flag.ENUM),
+        for (_ ← "module".!) yield Some(Classfile.Flag.MODULE)
+      ).reduce(_ | _)
+
+      val superclass = P { "extends" ~ WS ~ PathParser }
+      val interfaces = P { "implements" ~ WS ~ PathParser.rep(min = 1, sep = WS.? ~ "," ~ WS.?) }
+
+      ClassFlagParser ~ classKeywordParser ~ WS ~ Literal ~ WS ~ superclass.? ~ WS.? ~ interfaces.?
+    }
+
+    P {
+      pkg ~ WS.? ~ version ~ WS.? ~ ClassPrefixParser.rep(sep = WS.?) ~ WS.? ~
+        classDescriptor ~ WS.? ~ "{" ~ WS.? ~
+        ClassContentParser.rep(sep = WS.?) ~ WS.? ~
+        "}"
+    } map {
+      case (pkg, (major, minor), prefixes, (flags, classTypeFlag, name, superType, interfaces), contents) =>
+        val classfile = {
+          val version = new Version(major.toInt, minor.toInt)
+          val fullName = new Path(pkg.asJava, name)
+          new Classfile(version, fullName, superType.orNull, interfaces.getOrElse(Nil).asJava)
+        }
+
+        classfile.getAccessFlags.addAll(flags)
+        classTypeFlag.foreach(classfile.getAccessFlags.add)
+
+        prefixes.foreach(_ apply classfile)
+        contents.foreach(_ apply classfile)
+
+        classfile
+    }
+  }
+}
+
+sealed trait ClassPrefix extends (Classfile => Unit)
+object ClassPrefix {
+  class SignaturePrefix(signature: String) extends ClassPrefix {
+    def apply(classfile: Classfile): Unit = classfile.setSignature(Optional.of(signature))
+  }
+
+  class SourcePrefix(source: String) extends ClassPrefix {
+    def apply(classfile: Classfile): Unit = classfile.setSource(Optional.of(source))
+  }
+
+  class DebugPrefix(source: String) extends ClassPrefix {
+    def apply(classfile: Classfile): Unit = classfile.setSourceDebug(Optional.of(source))
+  }
+
+  class AnnotationPrefix(anno: Annotation) extends ClassPrefix {
+    def apply(classfile: Classfile): Unit = classfile.getAnnotations().add(anno)
+  }
+
+  class TypeAnnotationPrefix(typeAnnotation: ClassTypeAnnotation) extends ClassPrefix {
+    def apply(classfile : Classfile): Unit = classfile.getTypeAnnotations().add(typeAnnotation)
+  }
+
+  class AttributePrefix(attribute: Attribute) extends ClassPrefix {
+    def apply(classfile: Classfile): Unit = classfile.getAttributes().add(attribute)
+  }
+}
+
+object ClassPrefixParser extends Parser[ClassPrefix] {
+  object SignaturePrefixParser extends Parser[ClassPrefix.SignaturePrefix] {
+    val parser: P[ClassPrefix.SignaturePrefix] = P {
+      for (signature ← "signature" ~ WS ~ StringLiteral ~ WS.? ~ ";")
+        yield new ClassPrefix.SignaturePrefix(signature)
+    } opaque "<signature>"
+  }
+
+  object SourcePrefixParser extends Parser[ClassPrefix.SourcePrefix] {
+    val parser: P[ClassPrefix.SourcePrefix] = P {
+      for (source ← "source" ~ WS ~ StringLiteral ~ WS.? ~ ";")
+        yield new ClassPrefix.SourcePrefix(source)
+    } opaque "<source-file>"
+  }
+
+  object DebugPrefixParser extends Parser[ClassPrefix.DebugPrefix] {
+    val parser: P[ClassPrefix.DebugPrefix] = P {
+      for (debug ← "debug" ~ WS ~ StringLiteral ~ WS.? ~ ";")
+        yield new ClassPrefix.DebugPrefix(debug)
+    } opaque "<debug-source>"
+  }
+
+  object AnnotationPrefixParser extends Parser[ClassPrefix.AnnotationPrefix] {
+    val parser: P[ClassPrefix.AnnotationPrefix] = P {
+      for (annotation ← AnnotationParser ~ WS.? ~ ";".?) yield new ClassPrefix.AnnotationPrefix(annotation)
+    } opaque "<annotation>"
+  }
+
+  object TypeAnnotationPrefixParser extends Parser[ClassPrefix.TypeAnnotationPrefix] {
+    val parser: P[ClassPrefix.TypeAnnotationPrefix] = P {
+      for (annotation ← ClassTypeAnnotationParser ~ WS.? ~ ";") yield new ClassPrefix.TypeAnnotationPrefix(annotation)
+    } opaque "<type-annotation>"
+  }
+
+  object AttributePrefixParser extends Parser[ClassPrefix.AttributePrefix] {
+    val parser: P[ClassPrefix.AttributePrefix] = P {
+      for(attribute ← AttributeParser ~ WS.? ~ ";".?) yield new ClassPrefix.AttributePrefix(attribute)
+    } opaque "<attribute>"
+  }
+
+  val parser: P[ClassPrefix] = P {
+    SignaturePrefixParser |
+      SourcePrefixParser |
+      DebugPrefixParser |
+      AnnotationPrefixParser |
+      TypeAnnotationPrefixParser |
+      AttributePrefixParser
+  }
+}
+
+trait ClassContent extends (Classfile => Unit)
+object ClassContent {
+  class ModuleContent(module: Module) extends ClassContent {
+    override def apply(clazz: Classfile): Unit = clazz.setModule(Optional.of(module))
+  }
+
+  class EnclosingContent(enclosing: Classfile.EnclosingMethod) extends ClassContent {
+    override def apply(clazz: Classfile): Unit = clazz.setEnclosingMethod(Optional.of(enclosing))
+  }
+
+  class InnerClassContent(innerClass: Classfile.InnerClass) extends ClassContent {
+    override def apply(clazz: Classfile): Unit = clazz.getInnerClasses.add(innerClass)
+  }
+
+  class NestHostContent(host: Path) extends ClassContent {
+    override def apply(clazz: Classfile): Unit = clazz.setNestHost(Optional.of(host))
+  }
+
+  class NestMemberContent(members: Seq[Path]) extends ClassContent {
+    override def apply(clazz: Classfile): Unit = clazz.getNestMembers.addAll(members.asJava)
+  }
+
+  class FieldContent(field: Field) extends ClassContent {
+    override def apply(clazz: Classfile): Unit = clazz.getFields.add(field)
+  }
+
+  class MethodContent(method: Method) extends ClassContent {
+    override def apply(clazz: Classfile): Unit = clazz.getMethods.add(method)
+  }
+}
+
+object ClassContentParser extends Parser[ClassContent] {
+  object ModuleContentParser extends Parser[ClassContent.ModuleContent] {
+    val parser: P[ClassContent.ModuleContent] = P {
+      for (module ← ModuleParser) yield new ClassContent.ModuleContent(module)
+    } opaque "<module>"
+  }
+
+  object EnclosingContentParser extends Parser[ClassContent.EnclosingContent] {
+    val parser: P[ClassContent.EnclosingContent] = P {
+      val name = "name" ~ WS.? ~ "=" ~ WS.? ~ StringLiteral ~ WS.? ~ ";"
+      val descriptor = "descriptor" ~ WS.? ~ "=" ~ WS.? ~ MethodDescriptorParser ~ WS.? ~ ";"
+
+      for ((owner, name, descriptor) ← "enclosing" ~ WS.? ~ PathParser ~ WS.? ~ "{" ~ WS.? ~ (name ~ WS.?).? ~ (descriptor ~ WS.?).? ~ "}")
+        yield new ClassContent.EnclosingContent(new EnclosingMethod(owner, Optional.ofNullable(name.orNull), Optional.ofNullable(descriptor.orNull)))
+    } opaque "<enclosing-method>"
+  }
+
+  object InnerClassContentParser extends Parser[ClassContent.InnerClassContent] {
+    val parser: P[ClassContent.InnerClassContent] = P {
+      sealed trait InnerContent extends (Classfile.InnerClass => Unit)
+      class Inner(inner: String) extends InnerContent {
+        override def apply(cinner: Classfile.InnerClass): Unit =
+          cinner.setInnerName(Optional.of(inner))
+      }
+      class Outer(outer: Path) extends InnerContent {
+        override def apply(cinner: Classfile.InnerClass): Unit =
+          cinner.setOuterName(Optional.of(outer))
+      }
+
+      val inner = for (inner ← "inner" ~ WS ~ StringLiteral ~ ";") yield new Inner(inner)
+      val outer = for (outer ← "outer" ~ WS ~ PathParser ~ ";") yield new Outer(outer)
+
+      for ((flags, name, content) ← InnerClassFlagParser ~ "inner" ~ WS ~ PathParser ~ WS.? ~ "{" ~ WS.? ~
+        (inner | outer).rep(sep = WS.?) ~ WS.? ~ "}") yield {
+        val inner = new Classfile.InnerClass(name)
+        inner.setFlags(flags)
+        content.foreach(_ apply inner)
+        new ClassContent.InnerClassContent(inner)
+      }
+    } opaque "<inner-class>"
+  }
+
+  object NestHostContentParser extends Parser[ClassContent.NestHostContent] {
+    val parser: P[ClassContent.NestHostContent] = P {
+      for (host ← "nest" ~ WS ~ "host" ~ WS ~ PathParser ~ WS.? ~ ";")
+        yield new ClassContent.NestHostContent(host)
+    } opaque "<nest-host>"
+  }
+
+  object NestMemberContentParser extends Parser[ClassContent.NestMemberContent] {
+    val parser: P[ClassContent.NestMemberContent] = P {
+      for (hosts ← "nest" ~ WS ~ "member" ~ WS ~ PathParser.rep(min = 1, sep = WS.? ~ "," ~ WS.?) ~ WS.? ~ ";")
+        yield new ClassContent.NestMemberContent(hosts)
+    } opaque "<nest-member>"
+  }
+
+  object FieldContentParser extends Parser[ClassContent.FieldContent] {
+    val parser: P[ClassContent.FieldContent] = P {
+      Fail
+    } opaque "<field>"
+  }
+
+  object MethodContentParser extends Parser[ClassContent.MethodContent] {
+    val parser: P[ClassContent.MethodContent] = P {
+      Fail
+    } opaque "<method>"
+  }
+
+  val parser: P[ClassContent] = P {
+    ModuleContentParser |
+      EnclosingContentParser |
+      InnerClassContentParser |
+      NestHostContentParser |
+      NestMemberContentParser |
+      FieldContentParser |
+      MethodContentParser
+  }
+}
---
+++ parser-core/src/test/scala/me/aki/tactical/core/parser/test/ClassfileTest.scala
@@ -0,0 +1,224 @@
+package me.aki.tactical.core.parser.test
+
+import java.util.{Optional, Set => JSet}
+
+import me.aki.tactical.core.Classfile.InnerClass
+import me.aki.tactical.core.annotation.Annotation
+
+import scala.collection.JavaConverters._
+import me.aki.tactical.core._
+import me.aki.tactical.core.`type`._
+import me.aki.tactical.core.parser.{ClassfileParser, InnerClassFlagParser}
+import me.aki.tactical.core.typeannotation._
+import me.aki.tactical.core.typeannotation.TargetType.Extends
+import me.aki.tactical.core.typeannotation.TypePath.Kind
+import org.scalatest.{FlatSpec, Matchers}
+
+class ClassfileTest extends FlatSpec with Matchers {
+  "The ClassfileParser" should "parse the most basic classes" in {
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |class Foo {}
+      """.stripMargin
+    } shouldEqual new Classfile(new Classfile.Version(54, 0), Path.of("com", "example", "Foo"), null, Nil.asJava)
+  }
+
+  it should "parse implementes and extends" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |class Foo extends java.lang.Object implements java.lang.Cloneable {}
+      """.stripMargin
+    }
+    classfile.getSupertype shouldEqual Path.of("java", "lang", "Object")
+    classfile.getInterfaces shouldEqual List(Path.of("java", "lang", "Cloneable")).asJava
+  }
+
+  it should "parse different class keywords" in {
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |interface Foo extends java.lang.Object {}
+      """.stripMargin
+    }.getFlag(Classfile.Flag.INTERFACE) shouldEqual true
+
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |@interface Foo extends java.lang.Object {}
+      """.stripMargin
+    }.getFlag(Classfile.Flag.ANNOTATION) shouldEqual true
+
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |enum Foo extends java.lang.Object {}
+      """.stripMargin
+    }.getFlag(Classfile.Flag.ENUM) shouldEqual true
+
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |module Foo extends java.lang.Object {}
+      """.stripMargin
+    }.getFlag(Classfile.Flag.MODULE) shouldEqual true
+  }
+
+  it should "parse class signatures" in {
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |signature "<T:Ljava/lang/Object;>Ljava/lang/Object;";
+        |class Foo {}
+      """.stripMargin
+    }.getSignature shouldEqual Optional.of("<T:Ljava/lang/Object;>Ljava/lang/Object;")
+  }
+
+  it should "parse source and debug info" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |source "Foo.java";
+        |debug "bar";
+        |class Foo {}
+      """.stripMargin
+    }
+
+    classfile.getSource shouldEqual Optional.of("Foo.java")
+    classfile.getSourceDebug shouldEqual Optional.of("bar")
+  }
+
+  it should "parse annotations" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |@java.lang.Deprecated[visible = true]();
+        |class Foo {}
+      """.stripMargin
+    }
+
+    classfile.getAnnotations shouldEqual List(new Annotation(Path.of("java", "lang", "Deprecated"), true)).asJava
+  }
+
+  it should "parse type annotations" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |#[path = { ? <1> }, target = extends, annotation = @java.lang.Deprecated[visible = true]()];
+        |class Foo {}
+      """.stripMargin
+    }
+
+    val typeAnnotation = {
+      val typePath = new TypePath(List(new Kind.WildcardBound(), new Kind.TypeArgument(1)).asJava)
+      val annotation = new Annotation(Path.of("java", "lang", "Object"), true)
+      new ClassTypeAnnotation(typePath, annotation, new Extends())
+    }
+
+    classfile.getTypeAnnotations shouldEqual List(typeAnnotation).asJava
+  }
+
+  it should "parse attributes" in {
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |attribute "foo" { 00 0F 00 FF }
+        |class Foo {
+        |    module com.example.Foo {}
+        |}
+      """.stripMargin
+    }.getAttributes shouldEqual List(new Attribute("foo", Array(0, 15, 0, -1))).asJava
+  }
+
+  it should "parse a module" in {
+    ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |class Foo {
+        |    module com.example.Foo {}
+        |}
+      """.stripMargin
+    }.getModule shouldEqual Optional.of(new Module(Path.of("com", "example", "Foo")))
+  }
+
+  it should "parse the enclosing method" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |class Foo {
+        |    enclosing com.example.Bar {
+        |        name = "baz";
+        |        descriptor = (int, long) void;
+        |    }
+        |}
+      """.stripMargin
+    }
+
+    val descriptor = new MethodDescriptor(List[Type](IntType.getInstance, LongType.getInstance).asJava, Optional.empty())
+    classfile.getEnclosingMethod shouldEqual Optional.of(new Classfile.EnclosingMethod(Path.of("com", "example", "Bar"), Optional.of("baz"), Optional.of(descriptor)))
+  }
+
+  it should "parse inner classes" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |class Foo {
+        |    public static final inner com.example.Bar$Baz {
+        |        inner "Baz";
+        |        outer com.example.Bar;
+        |    }
+        |}
+      """.stripMargin
+    }
+
+    val innerClass = {
+      val flags = JSet.of[InnerClass.Flag](InnerClass.Flag.PUBLIC, InnerClass.Flag.FINAL, InnerClass.Flag.STATIC)
+      new InnerClass(Path.of("com", "example", "Bar$Baz"), Optional.of(Path.of("com", "example", "Bar")), Optional.of("Baz"), flags)
+    }
+    classfile.getInnerClasses shouldEqual List(innerClass).asJava
+  }
+
+  it should "parse the nest host" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |class Foo {
+        |    nest host com.example.Bar;
+        |}
+      """.stripMargin
+    }
+    classfile.getNestHost shouldEqual Optional.of(Path.of("com", "example", "Bar"))
+  }
+
+  it should "parse the nest members" in {
+    val classfile = ClassfileParser.parse {
+      """package com.example;
+        |version 54.0;
+        |
+        |class Foo {
+        |    nest member com.example.Bar, com.example.Baz;
+        |}
+      """.stripMargin
+    }
+    classfile.getNestMembers shouldEqual List(Path.of("com", "example", "Bar"), Path.of("com", "example", "Baz")).asJava
+  }
+
+  //TODO Fields
+  //TODO Methods
+}
