66ee5003322a0d56768e2dd165913c4d89332c00
aki_ks <aki@kaysubs.de>
2018-10-15 03:02:54 +0000

    Extend constant parser

--- parser-core/src/main/scala/me/aki/tactical/core/parser/constant.scala
+++ parser-core/src/main/scala/me/aki/tactical/core/parser/constant.scala
@@ -1,7 +1,15 @@
 package me.aki.tactical.core.parser
 
+import scala.collection.JavaConverters._
 import fastparse.all._
+
+import me.aki.tactical.core.`type`.RefType
 import me.aki.tactical.core.constant._
+
+object NullConstantParser extends Parser[NullConstant] {
+  val parser: P[NullConstant] =
+    for (_ ← "null".!) yield NullConstant.getInstance
+}
 
 object IntConstantParser extends Parser[IntConstant] {
   val parser: P[IntConstant] =
@@ -28,6 +36,37 @@ object StringConstantParser extends Parser[StringConstant] {
     for (string ← StringLiteral) yield new StringConstant(string)
 }
 
+object ClassConstantParser extends Parser[ClassConstant] {
+  val parser: P[ClassConstant] =
+    for (clazz ← ClassLiteral if clazz.isInstanceOf[RefType]) yield new ClassConstant(clazz.asInstanceOf[RefType])
+}
+
+object MethodTypeConstantParser extends Parser[MethodTypeConstant] {
+  val parser: P[MethodTypeConstant] =
+    for (desc ← MethodDescriptorParser) yield new MethodTypeConstant(desc.getParameterTypes, desc.getReturnType)
+}
+
+object HandleConstantParser extends Parser[HandleConstant] {
+  val parser: P[HandleConstant] =
+    for (handle ← "handle" ~ WS.? ~ "{" ~ WS.? ~ HandleParser ~ WS.? ~ "}") yield new HandleConstant(handle)
+}
+
+object DynamicConstantParser extends Parser[DynamicConstant] {
+  val parser: P[DynamicConstant] = P {
+    val arguments = "[" ~ WS.? ~ BootstrapConstantParser.rep(min = 0, sep = WS.? ~ "," ~ WS.?) ~ WS.? ~ "]"
+
+    for {
+      (name, typ, bootstrap, arguments) ←
+        "dynamic" ~ WS.? ~ "{" ~ WS.? ~
+          "name" ~ WS.? ~ "=" ~ WS.? ~ StringLiteral ~ WS.? ~ "," ~ WS.? ~
+          "type" ~ WS.? ~ "=" ~ WS.? ~ TypeParser ~ WS.? ~ "," ~ WS.? ~
+          "bootstrap" ~ WS.? ~ "=" ~ WS.? ~ HandleParser ~ WS.? ~ "," ~ WS.? ~
+          "arguments" ~ WS.? ~ "=" ~ WS.? ~ arguments ~ WS.? ~
+        "}"
+    } yield new DynamicConstant(name, typ, bootstrap, arguments.asJava)
+  }
+}
+
 object FieldConstantParser extends Parser[FieldConstant] {
   val parser: P[FieldConstant] = P {
     StringConstantParser |
@@ -35,5 +74,33 @@ object FieldConstantParser extends Parser[FieldConstant] {
     DoubleConstantParser |
     LongConstantParser |
     IntConstantParser
+  }
+}
+
+object PushableConstantParser extends Parser[PushableConstant] {
+  val parser: P[PushableConstant] = P {
+    NullConstantParser |
+    FloatConstantParser |
+    DoubleConstantParser |
+    LongConstantParser |
+    IntConstantParser |
+    StringConstantParser |
+    ClassConstantParser |
+    DynamicConstantParser |
+    HandleConstantParser |
+    MethodTypeConstantParser
+  }
+}
+
+object BootstrapConstantParser extends Parser[BootstrapConstant] {
+  val parser: P[BootstrapConstant] = P {
+    IntConstantParser |
+      LongConstantParser |
+      FloatConstantParser |
+      DoubleConstantParser |
+      StringConstantParser |
+      ClassConstantParser |
+      HandleConstantParser |
+      MethodTypeConstantParser
   }
 }
--- parser-core/src/test/scala/me/aki/tactical/core/parser/test/ConstantTest.scala
+++ parser-core/src/test/scala/me/aki/tactical/core/parser/test/ConstantTest.scala
@@ -1,9 +1,15 @@
 package me.aki.tactical.core.parser.test
 
+import java.util.Optional
+
+import scala.collection.JavaConverters._
+import me.aki.tactical.core.{FieldRef, MethodRef, Path}
+import me.aki.tactical.core.`type`._
 import me.aki.tactical.core.constant._
-import me.aki.tactical.core.parser.FieldConstantParser
+import me.aki.tactical.core.handle.{InvokeInterfaceHandle, InvokeStaticHandle, SetStaticHandle}
+import me.aki.tactical.core.parser._
 import org.scalatest.prop.PropertyChecks
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest._
 
 class ConstantTest extends FlatSpec with Matchers with PropertyChecks {
   "The FieldConstantParser" should "parse int values" in {
@@ -40,5 +46,40 @@ class ConstantTest extends FlatSpec with Matchers with PropertyChecks {
     }
   }
 
+  "The PushableConstantParser" should "parse 'null' constants" in {
+    PushableConstantParser.parse("null") shouldEqual NullConstant.getInstance
+  }
 
+  it should "parse class constants" in {
+    PushableConstantParser.parse("java.lang.String.class") shouldEqual new ClassConstant(new ObjectType(Path.STRING))
+    PushableConstantParser.parse("java.lang.String[][].class") shouldEqual new ClassConstant(new ArrayType(new ObjectType(Path.STRING), 2))
+  }
+
+  it should "parse method type constants" in {
+    PushableConstantParser.parse("() void") shouldEqual new MethodTypeConstant(Nil.asJava, Optional.empty[Type])
+    PushableConstantParser.parse("(int, long) double") shouldEqual new MethodTypeConstant(List[Type](IntType.getInstance, LongType.getInstance).asJava, Optional.of(DoubleType.getInstance))
+  }
+
+  it should "parse handle constants" in {
+    PushableConstantParser.parse("handle { set static java.lang.System.out : java.io.PrintStream }") shouldEqual
+      new HandleConstant(new SetStaticHandle(new FieldRef(Path.of("java", "lang", "System"), "out", new ObjectType(Path.of("java", "io", "PrintStream")))))
+
+    PushableConstantParser.parse("handle { invoke interface java.io.PrintStream.println(int) : void }") shouldEqual
+      new HandleConstant(new InvokeInterfaceHandle(new MethodRef(Path.of("java", "io", "PrintStream"), "println", List[Type](IntType.getInstance).asJava, Optional.empty[Type])))
+  }
+
+  it should "parse dynamic constants" in {
+    val constant = PushableConstantParser.parse(
+      """dynamic {
+        |  name = "foo",
+        |  type = int,
+        |  bootstrap = invoke static java.lang.Integer.parseInt(java.lang.String) : int,
+        |  arguments = [ 10, 20, "bar"]
+        |}
+      """.stripMargin.trim)
+
+    val bootstrapMethod = new MethodRef(Path.of("java", "lang", "Integer"), "parseInt", List[Type](new ObjectType(Path.STRING)).asJava, Optional.of(IntType.getInstance))
+    val arguments = List[BootstrapConstant](new IntConstant(10), new IntConstant(20), new StringConstant("bar"))
+    constant shouldEqual new DynamicConstant("foo", IntType.getInstance, new InvokeStaticHandle(bootstrapMethod, false), arguments.asJava)
+  }
 }
