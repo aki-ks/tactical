bafa328d522006388be37242ab645501aa934c6f
aki_ks <aki@kaysubs.de>
2018-11-06 11:25:32 +0100

    Add LinkedInsertList

--- core/src/main/java/me/aki/tactical/core/util/LinkedIdentityInsertList.java
+++ core/src/main/java/me/aki/tactical/core/util/LinkedIdentityInsertList.java
@@ -6,13 +6,14 @@ import java.util.IdentityHashMap;
  * A linked {@link InsertList} that finds elements in the collection based on identity.
  *
  * @param <T> type of elements contained within this list
+ * @see LinkedInsertList linked insert list that finds elements based on equality
  */
 public class LinkedIdentityInsertList<T> extends AbstractLinkedInsertList<T> {
     public LinkedIdentityInsertList() {
         super(new IdentityHashMap<>());
     }
 
-    public LinkedIdentityInsertList(Iterable elements) {
-        super(new IdentityHashMap(), elements);
+    public LinkedIdentityInsertList(Iterable<T> elements) {
+        super(new IdentityHashMap<>(), elements);
     }
 }
---
+++ core/src/main/java/me/aki/tactical/core/util/LinkedInsertList.java
@@ -0,0 +1,19 @@
+package me.aki.tactical.core.util;
+
+import java.util.HashMap;
+
+/**
+ * A implementation of {@link InsertList} based on a linked list that finds elements by equality, not identity.
+ *
+ * @param <T> type of elements in the list
+ * @see LinkedIdentityInsertList linked insert list that finds elements based on identity.
+ */
+public class LinkedInsertList<T> extends AbstractLinkedInsertList<T> {
+    public LinkedInsertList() {
+        super(new HashMap<>());
+    }
+
+    public LinkedInsertList(Iterable<T> elements) {
+        super(new HashMap<>(), elements);
+    }
+}
