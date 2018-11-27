e18d7c49faf8f0ec9da12ad4ef4fcf12e0fbb4ea
aki_ks <aki@kaysubs.de>
2018-11-27 16:21:30 +0100

    Improve Stack2Asm facade

--- conversion/asm-stack/src/main/java/me/aki/tactical/conversion/Stack2Asm.java
+++ conversion/asm-stack/src/main/java/me/aki/tactical/conversion/Stack2Asm.java
@@ -4,6 +4,8 @@ import me.aki.tactical.conversion.stack2asm.TacticalClassReader;
 import me.aki.tactical.core.Classfile;
 import me.aki.tactical.stack.StackBody;
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.tree.ClassNode;
 
 /**
@@ -13,6 +15,9 @@ import org.objectweb.asm.tree.ClassNode;
 public class Stack2Asm {
     /**
      * Make a {@link ClassVisitor} visit a {@link Classfile}.
+     *
+     * Note: Neither the {@link MethodVisitor#visitMaxs(int, int) stack size and local count} nor
+     * {@link MethodVisitor#visitFrame(int, int, Object[], int, Object[]) the frames} are visited.
      *
      * @param file the file that the {@link ClassVisitor} will visit.
      * @param cv the ClassVisitor that will visit the class
@@ -27,9 +32,21 @@ public class Stack2Asm {
      * @param classfile the {@link Classfile} to convert
      * @return the converted ASM {@link ClassNode}.
      */
-    public static ClassNode convert(Classfile classfile) {
+    public static ClassNode toClassNode(Classfile classfile) {
         ClassNode cn = new ClassNode();
         accept(classfile, cn);
         return cn;
+    }
+
+    /**
+     * Convert a {@link Classfile} into a byte array of jvm bytecode.
+     *
+     * @param classfile the {@link Classfile} to convert
+     * @return the converted jvm bytecode
+     */
+    public static byte[] toBytecode(Classfile classfile) {
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        accept(classfile, cw);
+        return cw.toByteArray();
     }
 }
