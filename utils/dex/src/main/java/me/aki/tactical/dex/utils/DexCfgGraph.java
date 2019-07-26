2cc05f4bf0824491ae871c612dd3fca0f73ffd36
aki-ks <aki@kaysubs.de>
2019-07-26 18:32:24 +0200

    Add Dex CFG anaylsis

---
+++ utils/dex/src/main/java/me/aki/tactical/dex/utils/DexCfgGraph.java
@@ -0,0 +1,58 @@
+package me.aki.tactical.dex.utils;
+
+import me.aki.tactical.core.utils.AbstractCfgGraph;
+import me.aki.tactical.dex.DexBody;
+import me.aki.tactical.dex.insn.BranchInstruction;
+import me.aki.tactical.dex.insn.Instruction;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DexCfgGraph extends AbstractCfgGraph<Instruction> {
+    private final DexBody body;
+
+    public DexCfgGraph(DexBody body) {
+        this.body = body;
+    }
+
+    @Override
+    protected Instruction getHeadInsn() {
+        return body.getInstructions().getFirst();
+    }
+
+    @Override
+    protected Collection<TryCatchBlock> getTryCatchBlocks() {
+        return body.getTryCatchBlocks().stream()
+                .flatMap(block -> block.getHandlers().stream()
+                        .map(handler -> new TryCatchBlock(block.getStart(), block.getEnd(), handler.getHandler())))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    protected Stream<Instruction> getReachableInstructions(Instruction instruction) {
+        Stream<Instruction> succeedingInsn = instruction.continuesExecution() ?
+                Stream.of(body.getInstructions().getNext(instruction)) : Stream.empty();
+
+        if (instruction instanceof BranchInstruction) {
+            Stream<Instruction> branchTargets = ((BranchInstruction) succeedingInsn).getBranchTargets().stream();
+            return Stream.concat(succeedingInsn, branchTargets);
+        } else {
+            return succeedingInsn;
+        }
+    }
+
+    @Override
+    public boolean isDeadCode(Instruction start, Instruction end) {
+        Iterator<Instruction> iterator = body.getInstructions().iterator(start, end);
+
+        while (iterator.hasNext()) {
+            if (!isDeadCode(iterator.next())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
